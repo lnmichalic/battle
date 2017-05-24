2c276930354c8362bac75635b2425f71451c24c9
Lubos Michalic <lnmichalic@gmail.com>
2017-05-24 15:14:37 +0100

    Refactor to implement the post redirect get pattern

--- app.rb
+++ app.rb
@@ -1,14 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post "/names" do
-    @name1 = params[:first_player_name]
-    @name2 = params[:second_player_name]
-    erb :names
+    session[:name1] = params[:first_player_name]
+    session[:name2] = params[:second_player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name1 = session[:name1]
+    @name2 = session[:name2]
+    erb :play
   end
 
   # start the server if ruby file executed directly
