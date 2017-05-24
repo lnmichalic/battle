0ae39927284d07c43a99d04577ed4ea2fa09987d
Lubos Michalic <lnmichalic@gmail.com>
2017-05-24 14:31:27 +0100

    First feature test for testing infrastructure, passing the test

---
+++ spec/features/infrastructure_test_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../../app'
+
+feature 'Battle' do
+
+  scenario 'reads Test infrastructure working on Homepage' do
+    visit('/')
+    expect(page).to have_content("Testing infrastructure working!")
+  end
+
+end
